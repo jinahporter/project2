c78d241d47a66ad9b8ea54a0f5095331d175bb5b
Jinah Porter <jinahporter@gmail.com>
2020-12-03 19:47:57 -0600

    add app.py flask file

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify
+
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import pandas as pd
+from config import password
+
+# SET UP THE DATABASE
+
+# variables to populate the database connection string
+db_user = 'postgres'
+db_password = password
+db_host = 'localhost'
+db_port = 5432
+# This database must already exist
+db_name = "youtube_database"
+
+engine = create_engine(f'postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+# reflect exisiting database into a new model
+Base = automap_base()
+
+# reflect tables
+Base.prepare(engine, reflect=True)
+results = Base.classes.youtube_table
+
+# making a change to reset cache
+
+# Flask SET UP
+
+app = Flask(__name__)
+
+# Flask Routes
+@app.route('/')
+def home():
+
+
+    session = Session(engine)
+
+    session_results = pd.read_sql(session.query(results.title, results.categoryId, results.view_count, results.likes, results.dislikes).statement, con=engine).to_dict(orient='records')
+    session.close()
+
+    return jsonify(session_results)
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
