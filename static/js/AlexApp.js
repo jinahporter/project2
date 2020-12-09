6eaea881713033e02d0a2aea9ef9ffdd2d9e143b
Alex Riner <rineralexander@gmail.com>
2020-12-08 20:10:54 -0600

    country buttons load data

--- static/js/AlexApp.js
+++ static/js/AlexApp.js
@@ -1,26 +1,41 @@
 var url = "/data/MX";
 var tbody = d3.select("tbody");
-d3.json(url).then(function(response) {
-  console.log(response);
-  tbody.text("");
+// d3.json(url).then(function(response) {
+//   console.log(response);
+//   tbody.text("");
 
-  response.forEach(entry => {
+//   response.forEach(entry => {
       
-    var row = tbody.append("tr");
+//     var row = tbody.append("tr");
   
-    Object.entries(entry).forEach(([key, value]) => {
+//     Object.entries(entry).forEach(([key, value]) => {
 
-      var cell = row.append("td");
-      cell.text(value);
+//       var cell = row.append("td");
+//       cell.text(value);
 
-    });
-  });
-});
+//     });
+//   });
+// });
 
 var usButton = d3.select("#US");
 usButton.on("click",function(){
-  url="/data/US"
-  d3.json(url).then(function(response) {
-    console.log(response);
-  })
+  countryUpdate(this.id);
 });
+var caButton = d3.select("#CA");
+caButton.on("click",function(){
+  countryUpdate(this.id);
+});
+var mxButton = d3.select("#MX");
+mxButton.on("click",function(){
+  countryUpdate(this.id);
+});
+var ukButton = d3.select("#UK");
+ukButton.on("click",function(){
+  countryUpdate(this.id);
+});
+function countryUpdate(cCode){
+  url=`/data/${cCode}`
+  d3.json(url).then(function(response) {
+      console.log(response);
+    })
+};
