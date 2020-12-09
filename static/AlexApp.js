47dc60691854fe0ad5f3fefe0026ea0977333928
Jinah Porter <jinahporter@gmail.com>
2020-12-08 20:43:12 -0600

    sorting files

--- static/AlexApp.js
+++ static/AlexApp.js
@@ -1,6 +1,7 @@
+
 var url = "/data/MX";
 var tbody = d3.select("tbody");
-d3.json(url).then(function(response) {
+/* d3.json(url).then(function(response) {
   console.log(response);
   tbody.text("");
 
@@ -15,4 +16,27 @@ d3.json(url).then(function(response) {
 
     });
   });
+}); */
+
+var usButton = d3.select("#US");
+usButton.on("click",function(){
+  countryUpdate(this.id);
 });
+var caButton = d3.select("#CA");
+caButton.on("click",function(){
+  countryUpdate(this.id);
+});
+var mxButton = d3.select("#MX");
+mxButton.on("click",function(){
+  countryUpdate(this.id);
+});
+var ukButton = d3.select("#UK");
+ukButton.on("click",function(){
+  countryUpdate(this.id);
+});
+function countryUpdate(cCode){
+  url=`/data/${cCode}`
+  d3.json(url).then(function(response) {
+      console.log(response);
+    })
+};
